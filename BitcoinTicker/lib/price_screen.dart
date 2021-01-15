044afe1c33c804bc8344835a9ff9b50ba63c10d9
Alexis <psialedev@gmail.com>
2021-01-15 12:04:48 -0600

    creating the PriceScreen file

---
+++ BitcoinTicker/lib/price_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  PriceScreen({Key key}) : super(key: key);
+
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
